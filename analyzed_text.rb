1331eb46bd3549d520ef0b8a99a69bef5eef9402
Chleboir-CZ <fkmj@volny.cz>
2017-06-12 11:28:50 +0200

    Fixed the generator
    Before, the following words after a word were new objects, and they
    had no follow-up(they were just Word objects with a string). This is now
    fixed. Also, an attribute 'ends_sentence' was added to Word.

--- analyzed_text.rb
+++ analyzed_text.rb
@@ -3,6 +3,24 @@ require_relative 'word'
 class AnalyzedText
 	def initialize(raw_string)
 		@words = parse_raw_string(raw_string)
+		@word_probability_sum = 0
+
+		@words.each_value do |word|
+			@word_probability_sum += word.probability
+		end
+	end
+
+	def get_markov_str(length)
+		word = get_first_word()
+		str = word.str
+		for x in 1..length
+			if (word = word.next) != nil
+				str = str + " " + word.str
+			else
+				break
+			end
+		end
+		return str
 	end
 
 	private
@@ -16,18 +34,40 @@ class AnalyzedText
 						if word == word_str
 							# increase probability of the word
 							word_obj.inc_prob
-							# get index of next word
-							index = split_str.index(word) + 1
-							# add to next words
-							if split_str[index] != nil
-								word_obj.add_next_word(split_str[index])
-							end
 						end
 					end
 					word_obj.dec_prob
 					return_hash[word_str] = word_obj
 				end
 			end
+			# add next words
+			for word in split_str
+				index = split_str.index(word)
+				if split_str[index + 1] != nil
+					return_hash[split_str[index]].add_next_word(return_hash[split_str[index + 1]])
+				end
+			end
 			return return_hash;
 		end
+
+		def get_first_word
+			rand_num = rand(1..@word_probability_sum)
+			i = 0
+			@words.each_value do |word|
+				if rand_num >= i and rand_num <= i + word.probability
+					return word
+				else
+					i += word.probability
+				end
+			end
+		end
 end
+
+
+raw_text = gets()
+if raw_text == nil
+	raise 'piču'
+end
+analyzed = AnalyzedText.new(raw_text)
+puts(analyzed.get_markov_str(50))
+
--- word.rb
+++ word.rb
@@ -4,6 +4,7 @@ class Word
 		@word = word
 		@possible_next_words = {}
 		@next_probabilities_sum = 0
+		@ends_sentence = (word[-1] == ".")
 	end
 
 	def inc_prob()
@@ -14,12 +15,39 @@ class Word
 		@probability -= 1
 	end
 
+	def probability()
+		return @probability
+	end
+
+	def ends_sentence
+		return @ends_sentence
+	end
+
+	def str
+		return @word
+	end
+	
+	# gets the continuing word in a sentence
+	def next()
+		rand_num = rand(1..@next_probabilities_sum)
+		i = 0
+		if @possible_next_words != nil
+			@possible_next_words.each_value do |word|
+				if rand_num >= i and rand_num <= i + word.probability
+					return word
+				else
+					i += word.probability
+				end
+			end
+		end
+		return nil
+	end
+
 	def add_next_word(word)
-		if @possible_next_words[word] == nil
-			word_obj = Word.new(word)
-			@possible_next_words[word] = word_obj
+		if @possible_next_words[word.str] == nil
+			@possible_next_words[word.str] = word
 		else
-			@possible_next_words[word].inc_prob()
+			@possible_next_words[word.str].inc_prob()
 		end
 		@next_probabilities_sum += 1
 	end
